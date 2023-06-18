110eba89d5b395070fc4f1d7cc16842e2768040c
Mohamed Ayman <128937934+S-tohy@users.noreply.github.com>
2023-06-18 12:05:07 +0300

    Add files via upload

---
+++ app/src/main/java/com/scanner/ahmed/MainActivity.java
@@ -0,0 +1,324 @@
+package com.scanner.ahmed;
+
+import com.scanner.ahmed.LohhhActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.*;
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.view.View.*;
+import android.widget.*;
+import android.content.*;
+import android.content.res.*;
+import android.graphics.*;
+import android.graphics.drawable.*;
+import android.media.*;
+import android.net.*;
+import android.text.*;
+import android.text.style.*;
+import android.util.*;
+import android.webkit.*;
+import android.animation.*;
+import android.view.animation.*;
+import java.util.*;
+import java.util.regex.*;
+import java.text.*;
+import org.json.*;
+import java.util.HashMap;
+import java.util.ArrayList;
+import android.widget.LinearLayout;
+import android.widget.EditText;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.os.Build;
+import androidx.core.content.FileProvider;
+import java.io.File;
+import android.os.Bundle;
+import java.io.InputStream;
+import android.app.Activity;
+import android.content.SharedPreferences;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ChildEventListener;
+import android.media.MediaPlayer;
+import android.text.Editable;
+import android.text.TextWatcher;
+import com.google.gson.Gson;
+import com.budiyev.android.codescanner.*;
+import com.google.zxing.*;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.DialogFragment;
+import androidx.core.content.ContextCompat;
+import androidx.core.app.ActivityCompat;
+import android.Manifest;
+import android.content.pm.PackageManager;
+
+
+public class MainActivity extends AppCompatActivity {
+	public final int REQ_CD_C = 101;
+	private FirebaseDatabase _firebase = FirebaseDatabase.getInstance();
+	
+	private  CodeScanner mCodeScanner;
+	private double index = 0;
+	private double pos = 0;
+	private HashMap<String, Object> map = new HashMap<>();
+	private double n = 0;
+	private String text = "";
+	private double len = 0;
+	private double exist = 0;
+	private String id = "";
+	private HashMap<String, Object> Map = new HashMap<>();
+	private boolean bo = false;
+	
+	private ArrayList<HashMap<String, Object>> user_name = new ArrayList<>();
+	private ArrayList<String> list = new ArrayList<>();
+	private ArrayList<HashMap<String, Object>> map2 = new ArrayList<>();
+	private ArrayList<HashMap<String, Object>> m = new ArrayList<>();
+	
+	private CodeScannerView scannerview;
+	private LinearLayout linear1;
+	private EditText edittext1;
+	
+	private Intent c = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+	private File _file_c;
+	private Intent i = new Intent();
+	private Intent intent = new Intent();
+	private SharedPreferences data;
+	private SharedPreferences sh;
+	private DatabaseReference UserArd = _firebase.getReference("UserArd");
+	private ChildEventListener _UserArd_child_listener;
+	private MediaPlayer mediaPlayer;
+	
+	@Override
+	protected void onCreate(Bundle _savedInstanceState) {
+		super.onCreate(_savedInstanceState);
+		setContentView(R.layout.main);
+		initialize(_savedInstanceState);
+		com.google.firebase.FirebaseApp.initializeApp(this);
+		if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED
+		|| ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED
+		|| ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+			ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1000);
+		}
+		else {
+			initializeLogic();
+		}
+	}
+	
+	@Override
+	public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+		if (requestCode == 1000) {
+			initializeLogic();
+		}
+	}
+	
+	private void initialize(Bundle _savedInstanceState) {
+		scannerview = (CodeScannerView) findViewById(R.id.scannerview);
+		linear1 = (LinearLayout) findViewById(R.id.linear1);
+		edittext1 = (EditText) findViewById(R.id.edittext1);
+		_file_c = FileUtil.createNewPictureFile(getApplicationContext());
+		Uri _uri_c = null;
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+			_uri_c= FileProvider.getUriForFile(getApplicationContext(), getApplicationContext().getPackageName() + ".provider", _file_c);
+		}
+		else {
+			_uri_c = Uri.fromFile(_file_c);
+		}
+		c.putExtra(MediaStore.EXTRA_OUTPUT, _uri_c);
+		c.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+		data = getSharedPreferences("data", Activity.MODE_PRIVATE);
+		sh = getSharedPreferences("sh", Activity.MODE_PRIVATE);
+		
+		edittext1.addTextChangedListener(new TextWatcher() {
+			@Override
+			public void onTextChanged(CharSequence _param1, int _param2, int _param3, int _param4) {
+				final String _charSeq = _param1.toString();
+				
+			}
+			
+			@Override
+			public void beforeTextChanged(CharSequence _param1, int _param2, int _param3, int _param4) {
+				
+			}
+			
+			@Override
+			public void afterTextChanged(Editable _param1) {
+				
+			}
+		});
+		
+		_UserArd_child_listener = new ChildEventListener() {
+			@Override
+			public void onChildAdded(DataSnapshot _param1, String _param2) {
+				GenericTypeIndicator<HashMap<String, Object>> _ind = new GenericTypeIndicator<HashMap<String, Object>>() {};
+				final String _childKey = _param1.getKey();
+				final HashMap<String, Object> _childValue = _param1.getValue(_ind);
+				UserArd.addListenerForSingleValueEvent(new ValueEventListener() {
+					@Override
+					public void onDataChange(DataSnapshot _dataSnapshot) {
+						user_name = new ArrayList<>();
+						try {
+							GenericTypeIndicator<HashMap<String, Object>> _ind = new GenericTypeIndicator<HashMap<String, Object>>() {};
+							for (DataSnapshot _data : _dataSnapshot.getChildren()) {
+								HashMap<String, Object> _map = _data.getValue(_ind);
+								user_name.add(_map);
+							}
+						}
+						catch (Exception _e) {
+							_e.printStackTrace();
+						}
+						if (!data.getString(_childKey, "").contains("name")) {
+							SketchwareUtil.showMessage(getApplicationContext(), "Created successfully");
+							data.edit().putString(_childKey, "name").commit();
+						}
+						else {
+							
+						}
+					}
+					@Override
+					public void onCancelled(DatabaseError _databaseError) {
+					}
+				});
+			}
+			
+			@Override
+			public void onChildChanged(DataSnapshot _param1, String _param2) {
+				GenericTypeIndicator<HashMap<String, Object>> _ind = new GenericTypeIndicator<HashMap<String, Object>>() {};
+				final String _childKey = _param1.getKey();
+				final HashMap<String, Object> _childValue = _param1.getValue(_ind);
+				
+			}
+			
+			@Override
+			public void onChildMoved(DataSnapshot _param1, String _param2) {
+				
+			}
+			
+			@Override
+			public void onChildRemoved(DataSnapshot _param1) {
+				GenericTypeIndicator<HashMap<String, Object>> _ind = new GenericTypeIndicator<HashMap<String, Object>>() {};
+				final String _childKey = _param1.getKey();
+				final HashMap<String, Object> _childValue = _param1.getValue(_ind);
+				
+			}
+			
+			@Override
+			public void onCancelled(DatabaseError _param1) {
+				final int _errorCode = _param1.getCode();
+				final String _errorMessage = _param1.getMessage();
+				
+			}
+		};
+		UserArd.addChildEventListener(_UserArd_child_listener);
+	}
+	
+	private void initializeLogic() {
+		CodeScannerView scannerView = findViewById(R.id.scannerview);
+		 mCodeScanner = new CodeScanner(this, scannerView);
+		mCodeScanner.setDecodeCallback(new DecodeCallback() {
+			    @Override public void onDecoded(@NonNull final Result result) { runOnUiThread(new Runnable() {
+					         @Override
+					          public void run() { 
+						          
+						       edittext1.setText(result.getText());
+						if (!"".equals(edittext1.getText().toString())) {
+							if (new Gson().toJson(user_name).contains(edittext1.getText().toString())) {
+								mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.sound);
+								mediaPlayer.start();
+								SketchwareUtil.showMessage(getApplicationContext(), "❌❌");
+							}
+							else {
+								SketchwareUtil.showMessage(getApplicationContext(), "Account creating please wait...");
+								Map = new HashMap<>();
+								Map.put("name", edittext1.getText().toString());
+								UserArd.push().updateChildren(Map);
+								Map.clear();
+							}
+						}
+						else {
+							SketchwareUtil.showMessage(getApplicationContext(), "Enter Anything!!!");
+						}
+						          
+						           } }
+				           
+				           ); }
+			           
+			            }
+		           
+		           );
+		scannerView.setOnClickListener(new View.OnClickListener() { @Override public void onClick(View view) { mCodeScanner.startPreview(); } }); 
+		edittext1.setVisibility(View.GONE);
+	}
+	
+	@Override
+	protected void onActivityResult(int _requestCode, int _resultCode, Intent _data) {
+		super.onActivityResult(_requestCode, _resultCode, _data);
+		switch (_requestCode) {
+			
+			default:
+			break;
+		}
+	}
+	
+	@Override
+	public void onBackPressed() {
+		finishAffinity();
+	}
+	@Deprecated
+	public void showMessage(String _s) {
+		Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Deprecated
+	public int getLocationX(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[0];
+	}
+	
+	@Deprecated
+	public int getLocationY(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[1];
+	}
+	
+	@Deprecated
+	public int getRandom(int _min, int _max) {
+		Random random = new Random();
+		return random.nextInt(_max - _min + 1) + _min;
+	}
+	
+	@Deprecated
+	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+		ArrayList<Double> _result = new ArrayList<Double>();
+		SparseBooleanArray _arr = _list.getCheckedItemPositions();
+		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+			if (_arr.valueAt(_iIdx))
+			_result.add((double)_arr.keyAt(_iIdx));
+		}
+		return _result;
+	}
+	
+	@Deprecated
+	public float getDip(int _input) {
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+	}
+	
+	@Deprecated
+	public int getDisplayWidthPixels() {
+		return getResources().getDisplayMetrics().widthPixels;
+	}
+	
+	@Deprecated
+	public int getDisplayHeightPixels() {
+		return getResources().getDisplayMetrics().heightPixels;
+	}
+}
